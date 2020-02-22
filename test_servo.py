329aab218804bd0d5db1f978aed53e57b9fcd079
bash <artem>
2020-02-22 20:39:38 +0000

    Test

--- test_servo.py
+++ test_servo.py
@@ -8,19 +8,18 @@ GPIO.setup(servoPIN, GPIO.OUT)
 GPIO.setup(secondPin, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(50) # Initialization
 pin = GPIO.PWM(secondPin, 50)
-pin.start(2.5)
+pin.start(50)
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    pin.ChangeDutyCycle(5)
-    time.sleep(0.5)
+    print("input p")
+    num1 = int(input())
+    p.ChangeDutyCycle(num1)
+    time.sleep(1)
+    print("input pin:")
+    num2 =int(input())
+    p.ChangeDutyCycle(num2)
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
