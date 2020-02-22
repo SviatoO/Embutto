234b2f403076e346f89b4960bf52dda99e185257
bash <artem>
2020-02-22 19:06:14 +0000

    test image

--- camera.py
+++ camera.py
@@ -83,9 +83,11 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
+    print("configuring")
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
+    print("Recording started")
     try:
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler)
